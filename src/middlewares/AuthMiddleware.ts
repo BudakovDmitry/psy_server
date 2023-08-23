a4bff596a048630719b4b431f73482071a20c8a4
dmitriy.budakov <dmitry.budakov@forforce.com>
2023-08-23 12:21:22 +0300

    Update Auth

---
+++ src/middlewares/AuthMiddleware.ts
@@ -0,0 +1,27 @@
+import ApiError from "../exceptions/ApiError.js";
+import TokenService from "../services/TokenService.js";
+
+const AuthMiddleware = (req: any, res: any, next: any) => {
+    try {
+        const authorizationHeader = req.headers.authorization;
+        if(!authorizationHeader) {
+            return next(ApiError.UnauthorizedError())
+        }
+
+        const accessToken = authorizationHeader.split(' ')[1];
+        if(!accessToken) {
+            return next(ApiError.UnauthorizedError())
+        }
+
+        const userData = TokenService.validateAccessToken(accessToken);
+        if(!userData) {
+            return next(ApiError.UnauthorizedError())
+        }
+        req.user = userData;
+        next();
+    } catch (e) {
+        return next(ApiError.UnauthorizedError())
+    }
+}
+
+export default AuthMiddleware;
--- src/routes/api/usersRouter.ts
+++ src/routes/api/usersRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import UserController from '../../controllers/UserController.js';
+import AuthMiddleware from "../../middlewares/AuthMiddleware.js";
 
 const usersRouter: Router = Router();
 
 usersRouter.post('', UserController.createUser);
-usersRouter.get('', UserController.getAllUsers);
+usersRouter.get('', AuthMiddleware, UserController.getAllUsers);
 usersRouter.get('/:id', UserController.getUser);
 
 export default usersRouter;
