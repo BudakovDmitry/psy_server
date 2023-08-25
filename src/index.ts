c378471f512de6c98b9dd5b47e6435dbaa4fcf02
dmitriy.budakov <dmitry.budakov@forforce.com>
2023-08-26 01:15:21 +0300

    Add websocket

--- src/index.ts
+++ src/index.ts
@@ -8,33 +8,56 @@ import AppRouter from './routes/index.js';
 import { options } from './swagger.js';
 // @ts-ignore
 import cookieParser from 'cookie-parser';
+import { Server } from 'socket.io';
+import http from 'http';
+
 
 const PORT = process.env.PORT || 5000;
 const DB_URL = process.env.MONGODB_URL || '';
-
-const app = express();
-const router = new AppRouter(app);
-
-const specs = swaggerJsDoc(options);
 
 const corsOptions = {
   origin: 'http://localhost:3000',
   credentials: true
 }
 
+const app = express();
+app.use(cors(corsOptions));
+
+const router = new AppRouter(app);
+
+const specs = swaggerJsDoc(options);
+
+
 app.set('port', process.env.PORT || 5000);
 app.use(express.json());
 app.use(cookieParser())
-app.use(cors(corsOptions));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 mongoose.set('strictQuery', false);
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket: any) => {
+  console.log('A user connected');
+
+  // Логіка обробки повідомлень і передача їх іншим клієнтам
+  socket.on('message', (message: any) => {
+    console.log(`Received message: ${message}`);
+    socket.broadcast.emit('message', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
 
 router.init();
 
 async function startApp() {
   try {
     await mongoose.connect(DB_URL);
-    app.listen(PORT, () => console.log(`SERVER STARTED ON PORT ${PORT}`));
+    server.listen(PORT, () => console.log(`SERVER STARTED ON PORT ${PORT}`));
     console.log('MongoDB Connected...');
   } catch (error) {
     console.log(error);
